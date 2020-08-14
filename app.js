679bc0c127cf2037c310e0a2220de63999f9fdc4
oohyuti <yuti0622@gmail.com>
2020-08-14 12:37:40 +0800

    修改port

--- app.js
+++ app.js
@@ -93,7 +93,8 @@ app.get('/API/addMember', (req, res) => {
 app.get('*', (req, res) => {  
     res.send('No Content');  
 });
+let port = process.env.port || 3000;
 
-app.listen(3000, () => {  
-    console.log('Listening on port 3000');  
+app.listen(port, () => {  
+    console.log('Listening on port'+ port);  
 }); 
